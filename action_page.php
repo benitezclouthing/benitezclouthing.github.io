3b35bdcec1ebed955920f04e11f764f464d45222
xCaldb24x <99769777+xCaldb24x@users.noreply.github.com>
2022-03-22 16:55:42 -0600

    Update action_page.php

--- action_page.php
+++ action_page.php
@@ -1,8 +1,17 @@
 <html>
 <body>
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
+Welcome <?php echo $_POST["name "]; ?><?php echo $_POST["lname"]; ?><br>
+  <?php echo $_POST["complaint"]; ?>
+Your email address is: <?php echo $_POST["email"]; ?><br>
+  <?php echo $_POST["complaint"]; ?><br>
+  <?php echo $_POST["number"]; ?><br>
+  <?php echo $_POST["date"]; ?><br>
 
+
+
+  
+
+  
 </body>
 </html>
